439fed6c467902bf5ff52bb6f7bcb6a8ba05968f
Kaede Fujisaki <kaede@hexe.net>
2020-10-01 17:11:50 +0900

    init vue

---
+++ fe/src/main.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+function main() {
+
+}
+
+main();
---
+++ fe/webpack.config.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import { Configuration } from 'webpack'
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/main.ts",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      vue: "vue/dist/vue.js"
+    }
+  }
+};
+
+export default config;
