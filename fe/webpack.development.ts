187a58d6a102b27cfc7377fb2701cd702d0515ab
Kaede Fujisaki <kaede@hexe.net>
2020-10-28 02:56:22 +0900

    clean up

--- fe/webpack.development.ts
+++
@@ -1,7 +0,0 @@
-import merge from 'webpack-merge';
-import common from './webpack.common';
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map'
-});
--- fe/webpack.production.ts
+++
@@ -1,7 +0,0 @@
-import { merge } from 'webpack-merge';
-import common from './webpack.common';
-
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-});
